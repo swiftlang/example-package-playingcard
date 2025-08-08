1790919605c61bc9c11ecc7bd0fa6e7779a5ea33
Joe Heck <j_heck@apple.com>
2025-08-08 09:56:00 -0700

    license header updates to align with latest expectations

--- Package.swift
+++ Package.swift
@@ -2,6 +2,17 @@
 
 import PackageDescription
 
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the example package playingcard open source project
+//
+// Copyright (c) 2015-2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
 let package = Package(
     name: "PlayingCard",
     platforms: [.macOS(.v10_15)],
--- Sources/PlayingCard/PlayingCard.swift
+++ Sources/PlayingCard/PlayingCard.swift
@@ -1,12 +1,13 @@
-/*
- This source file is part of the Swift.org open source project
-
- Copyright 2015 Apple Inc. and the Swift project authors
- Licensed under Apache License v2.0 with Runtime Library Exception
-
- See http://swift.org/LICENSE.txt for license information
- See http://swift.org/CONTRIBUTORS.txt for Swift project authors
-*/
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the example package playingcard open source project
+//
+// Copyright (c) 2015-2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
 
 /// The representation of a playing card.
 ///
